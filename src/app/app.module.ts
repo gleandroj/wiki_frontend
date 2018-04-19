1515c7ec0ce546324f2436c2a46cd8a6eafc9cf1
Gabriel Leandro <gabrielleandrojunior@live.com>
2018-04-19 15:26:42 -0300

    ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './customer/customer-list.component';
 import { PaginationModule } from 'ngx-bootstrap';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app.routing';
 import { CustomerFormComponent } from './customer/customer-form.component';
@@ -22,6 +22,7 @@ import { CustomerService } from './customer/customer-service';
     PaginationModule.forRoot(),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [CustomerService],
